ddafd4dd374afb4273bc0d1bb8af7260dfa10a04
Hossein Samani <samanihossein1379@gmail.com>
2022-07-08 22:30:35 +0430

    ExpenseItem wrapped into li

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -4,13 +4,15 @@ import "../assets/style/expense-item.css";
 const ExpenseItem = ({ title, amount, date }) => {
   const _amount = amount.toLocaleString("en-US");
   return (
-    <div className="expense-item">
-      <ExpenseDate date={date} />
-      <div className="expense-item__description">
-        <h2>{title}</h2>
-        <div className="expense-item__price">${_amount}</div>
+    <li>
+      <div className="expense-item">
+        <ExpenseDate date={date} />
+        <div className="expense-item__description">
+          <h2>{title}</h2>
+          <div className="expense-item__price">${_amount}</div>
+        </div>
       </div>
-    </div>
+    </li>
   );
 };
 
