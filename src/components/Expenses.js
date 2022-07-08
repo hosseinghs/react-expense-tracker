b53c649ac47161db787a6820ecd52834600f8cba
Hossein Samani <samanihossein1379@gmail.com>
2022-07-08 19:48:12 +0430

    ExpenseFilter added to Expense component

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,13 @@
 import ExpenseItem from "./ExpenseItem";
+import ExpenseFilter from "./ExpenseFilter";
 import "../assets/style/expenses.css";
 
-const Expense = ({mockData}) => {
- 
+const Expense = ({ mockData }) => {
   return (
     <div className="expenses">
+      <div>
+        <ExpenseFilter />
+      </div>
       {mockData.map(({ id, title, amount, date }) => (
         <ExpenseItem key={id} title={title} amount={amount} date={date} />
       ))}
