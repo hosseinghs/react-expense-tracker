b8fe848bd142cb592831a8d165be84f6bea23c0a
Hossein Samani <samanihossein1379@gmail.com>
2022-07-08 22:30:11 +0430

    expenselist component added

---
+++ src/components/ExpenseList.js
@@ -0,0 +1,17 @@
+import ExpenseItem from "./ExpenseItem";
+import "../assets/style/expense-list.css";
+
+const ExpenseList = ({ items }) => {
+  if (items.length === 0)
+    return <h2 className="expenses-list__fallback">Found no Data!</h2>;
+
+  return (
+    <ul className="expenses-list">
+      {items.map(({ id, title, amount, date }) => (
+        <ExpenseItem key={id} title={title} amount={amount} date={date} />
+      ))}
+    </ul>
+  );
+};
+
+export default ExpenseList;
