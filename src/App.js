d15f81a61bc7ec8b48568776d3b75205fa0202fc
Hossein Samani <samanihossein1379@gmail.com>
2022-07-08 19:48:30 +0430

    mock data randomized a little =))

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpense";
 function App() {
   const [mockData, setMockData] = useState([
-    { id: 0, title: "car insurance", amount: 2000, date: "2021-4-22" },
+    { id: 0, title: "car insurance", amount: 2000, date: "2020-4-22" },
     { id: 1, title: "health insurance", amount: 175, date: "2021-2-25" },
-    { id: 2, title: "rent", amount: 5000, date: "2021-5-1" },
+    { id: 2, title: "rent", amount: 5000, date: "2022-5-1" },
   ]);
 
   const saveExpenseItem = (expense) => {
