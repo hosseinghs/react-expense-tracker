4e23350bb564eeb4737c094187e9adebb2947c35
Hossein Samani <samanihossein1379@gmail.com>
2022-07-08 21:20:24 +0430

    bug fixed

--- src/App.js
+++ src/App.js
@@ -2,20 +2,20 @@ import { useState } from "react";
 import Expenses from "./components/Expenses";
 import NewExpense from "./components/NewExpense";
 function App() {
-  const [mockData, setMockData] = useState([
+  const [expenses, setExpenses] = useState([
     { id: 0, title: "car insurance", amount: 2000, date: "2020-4-22" },
     { id: 1, title: "health insurance", amount: 175, date: "2021-2-25" },
     { id: 2, title: "rent", amount: 5000, date: "2022-5-1" },
   ]);
 
   const saveExpenseItem = (expense) => {
-    console.log(mockData);
+    setExpenses([expense, ...expenses]);
   };
 
   return (
     <>
       <NewExpense onSaveExpenseData={saveExpenseItem} />
-      <Expenses mockData={mockData} />
+      <Expenses mockData={expenses} />
     </>
   );
 }
