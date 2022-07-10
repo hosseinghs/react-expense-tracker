026825019e1b903d677c0cc93b3527f6ad3244f0
Hossein Samani <samanihossein1379@gmail.com>
2022-07-10 11:15:27 +0430

    btn added to newExpense component

--- src/components/NewExpense.js
+++ src/components/NewExpense.js
@@ -5,6 +5,7 @@ const NewExpense = ({ onSaveExpenseData }) => {
   const [title, setTitle] = useState("");
   const [amount, setAmount] = useState("");
   const [date, setDate] = useState("");
+  const [isAdding, setIsAdding] = useState(false);
 
   const clearStates = () => {
     setTitle("");
@@ -21,43 +22,56 @@ const NewExpense = ({ onSaveExpenseData }) => {
       date: new Date(date),
     };
     onSaveExpenseData(expense);
+    setIsAdding(false);
     clearStates();
   };
 
   return (
-    <form className="new-expense" onSubmit={submitForm}>
-      <div className="new-expense__controls">
-        <div className="new-expense__control">
-          <label>Title</label>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+    <>
+      {!isAdding && (
+        <div className="new-expense">
+          <button onClick={() => setIsAdding(true)}>Add New Expense</button>
         </div>
-        <div className="new-expense__control">
-          <label>Amount</label>
-          <input
-            type="number"
-            value={amount}
-            onChange={(e) => setAmount(e.target.value)}
-          />
-        </div>
-        <div className="new-expense__control">
-          <label>Date</label>
-          <input
-            type="date"
-            min="2019-01-01"
-            max="2022-12-31"
-            value={date}
-            onChange={(e) => setDate(e.target.value)}
-          />
-        </div>
-        <div className="new-expense__actions">
-          <button type="submit">Add Expense</button>
-        </div>
-      </div>
-    </form>
+      )}
+      {isAdding && (
+        <form className="new-expense" onSubmit={submitForm}>
+          <div className="new-expense__controls">
+            <div className="new-expense__control">
+              <label>Title</label>
+              <input
+                type="text"
+                value={title}
+                onChange={(e) => setTitle(e.target.value)}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Amount</label>
+              <input
+                type="number"
+                value={amount}
+                onChange={(e) => setAmount(e.target.value)}
+              />
+            </div>
+            <div className="new-expense__control">
+              <label>Date</label>
+              <input
+                type="date"
+                min="2019-01-01"
+                max="2022-12-31"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
+            </div>
+            <div className="new-expense__actions">
+              <button type="button" onClick={() => setIsAdding(false)}>
+                Cancel
+              </button>
+              <button type="submit">Add Expense</button>
+            </div>
+          </div>
+        </form>
+      )}
+    </>
   );
 };
 
