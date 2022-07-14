937c881a7f377510e01fad14951b7c1d298f5689
Hossein Samani <samanihossein1379@gmail.com>
2022-07-14 22:43:55 +0430

    mockdata changed

--- src/App.js
+++ src/App.js
@@ -3,9 +3,25 @@ import Expense from "./components/Expense";
 import NewExpense from "./components/Expense/NewExpense";
 function App() {
   const [expenses, setExpenses] = useState([
-    { id: 0, title: "car insurance", amount: 2000, date: "2020-4-22" },
-    { id: 1, title: "health insurance", amount: 175, date: "2021-2-25" },
-    { id: 2, title: "rent", amount: 5000, date: "2022-5-1" },
+    {
+      id: "e1",
+      title: "Toilet Paper",
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+    },
+    { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+    {
+      id: "e3",
+      title: "Car Insurance",
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+    },
+    {
+      id: "e4",
+      title: "New Desk (Wooden)",
+      amount: 450,
+      date: new Date(2021, 5, 12),
+    },
   ]);
 
   const saveExpenseItem = (expense) => {
