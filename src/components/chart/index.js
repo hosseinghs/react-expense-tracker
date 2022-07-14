f344a69bb13224bd1f7d868ca463f4f01358179a
Hossein Samani <samanihossein1379@gmail.com>
2022-07-14 16:07:49 +0430

    maxValue passed into BarChart component

--- src/components/chart/index.js
+++ src/components/chart/index.js
@@ -2,13 +2,16 @@ import Bar from "./Bar";
 import "../../assets/style/chart/chart.css";
 
 const Chart = ({ items }) => {
+  const dataPoinstValue = items.map((item) => item.value);
+  const max = Math.max(...dataPoinstValue);
+  
   return (
     <div className="chart">
       {items.map((item, i) => (
         <Bar
           key={i}
           value={item.value}
-          maxValue={item.maxValue}
+          maxValue={max}
           label={item.label}
         />
       ))}
